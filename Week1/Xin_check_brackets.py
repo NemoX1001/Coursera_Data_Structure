bdc2706b844003dfa07299fb40a69c250720490b
NemoX1001 <415865585@qq.com>
2017-08-29 23:08:32 +0800

    Update Xin_check_brackets.py

--- Week1/Xin_check_brackets.py
+++ Week1/Xin_check_brackets.py
@@ -19,9 +19,15 @@ if __name__ == "__main__":
     text = sys.stdin.read()
 
     opening_brackets_stack = []
+    
+    #遍历该text中所有的文字
     for i, item in enumerate(text):
+        #如果是（，{，[，储存在stack中
         if item == '(' or item == '[' or item == '{':
             opening_brackets_stack.append(Bracket(item, i))
+        #如果是），}，]. 
+        #若原数组为空，跳出循环并报没有匹配的item的位置
+        #若原数组不为空，则把原数组最后一个值放入last_item中。判断last_item和item中的type相同。若不相同，则记录在opening_brackets_stack中
         if item == ')' or item == ']' or item == '}':
             if len(opening_brackets_stack) == 0:
                 opening_brackets_stack.append(Bracket(item, i))
