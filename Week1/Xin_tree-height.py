36b529fcd922c769346f51ee771ad7dcca1a206d
NemoX1001 <415865585@qq.com>
2017-09-06 22:38:28 +0800

    Update Xin_tree-height.py

--- Week1/Xin_tree-height.py
+++ Week1/Xin_tree-height.py
@@ -26,19 +26,26 @@ class TreeHeight:
                     self.root = nodes[i]
                 else:
                     nodes[self.parent[i]].addChild(nodes[i])
+
+            #生成一个基本队列q LILO，队列中存放的是树中同一层的 node
             q = queue.Queue()
+            #放入root的值到q，从 root 开始一层一层的遍历这棵树，当前是 root 所在的层，所以只有 root 一个 node
             q.put(self.root)
+            #初始高度
             height = 0
             while(not q.empty()):
                 size = q.qsize()
                 if size > 0:
                     height = height + 1
+                # 然后把当前层中所有 node 的 children 放入队列，并同时依次排空当前层的 node
+                # 也就是说，当前 for 循环之后，这一层的所有 node 都被 get 出来了。
+                # 当此 for 循环结束后，也就是下一次 while 循环开始前，队列中就只有下一层 node 了
                 for j in range(size):
                     item = q.get()
                     for i in item.children:
                         q.put(i)
             return height
-
+            #也就是说下一次while的时候遍历的是下一层
 
 def main():
   tree = TreeHeight()
